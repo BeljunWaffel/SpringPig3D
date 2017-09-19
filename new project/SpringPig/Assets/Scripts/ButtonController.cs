f640bc51b5e54ceb9bf85777fc762549e773a047
Jack <jfansea@gmail.com>
2017-09-18 21:08:25 -0700

    Buttons and Gates
    Created prefabs for buttons and gates, as well as the scripts to control them

---
+++ new project/SpringPig/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    public bool activated = false;
+    private Rigidbody button;
+
+	// Use this for initialization
+	void Start () {
+
+        button = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // TODO: Some better way to show a pushed button
+            button.gameObject.SetActive(false);
+
+            activated = true;
+        }
+    }
+}
