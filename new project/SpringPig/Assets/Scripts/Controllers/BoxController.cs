3eb93c4801f4dd54f40dc03fce5b4cc6ad7afaa1
Marc-Antoine Fontenelle <ma.fontenelle@hotmail.com>
2017-11-08 00:04:51 -0800

    fixed box movement to not be based on user input anymore. Now based on player velocity instead.

--- new project/SpringPig/Assets/Scripts/Controllers/BoxController.cs
+++ new project/SpringPig/Assets/Scripts/Controllers/BoxController.cs
@@ -9,18 +9,6 @@ public class BoxController : MonoBehaviour
         _box = GetComponent<Rigidbody>();
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (IsCollidingWithPlayer(collision))
-        {
-            var player = collision.gameObject.GetComponent<PlayerController>();
-            if (IsPlayerAtSameLevelAsBox(player))
-            {
-                PushBox(player);
-            }
-        }
-    }
-
     void OnCollisionStay(Collision collision)
     {
         if (IsCollidingWithPlayer(collision))
@@ -70,19 +58,15 @@ public class BoxController : MonoBehaviour
             var collisionDirection = new Vector3(positionDiff.x * -1, 0, positionDiff.z * -1).normalized;
 
             var isCollisionX = Mathf.Abs(collisionDirection.x) > Mathf.Abs(collisionDirection.z);
-
-            // Verify player input
-            var moveHorizontal = Input.GetAxis("Horizontal");
-            var moveVertical = Input.GetAxis("Vertical");
-
+            
             // Set box push speed to 1
-            if (isCollisionX && moveHorizontal != 0 && MathUtils.IsSameSign(moveHorizontal, collisionDirection.x))
+            if (isCollisionX && player.velocity.x != 0 && MathUtils.IsSameSign(player.velocity.x, collisionDirection.x))
             {
                 _box.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
                 _box.velocity = new Vector3(player.velocity.x, 0f, 0f);
                 playerObject.IsPushingBoxInX = true;
             }
-            else if (!isCollisionX && moveVertical != 0 && MathUtils.IsSameSign(moveVertical, collisionDirection.z))
+            else if (!isCollisionX && player.velocity.z != 0 && MathUtils.IsSameSign(player.velocity.z, collisionDirection.z))
             {
                 _box.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX;
                 _box.velocity = new Vector3(0f, 0f, player.velocity.z);
